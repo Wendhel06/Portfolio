6bf1fcf2b35d19778b4c2bc85789d283883d6d69
Wendhel Alves Nogueira <wendhelalves7@gmail.com>
2023-03-13 20:55:05 -0300

    Adicionei as rotas do projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
 
 const App = () => {
   return (
-    <div>Aplicativo funcionando</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route  path='/' element={<Home />}/>
+          </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div> 
   )
 }
 
